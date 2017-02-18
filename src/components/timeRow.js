e5b808dcb6276986d04e62070c0a30c7f428f1f0
VladDev26 <volodimirb23@gmail.com>
2017-02-18 17:15:45 +0200

    0.1

--- src/components/timeRow.js
+++ src/components/timeRow.js
@@ -1,44 +1,24 @@
 import React from 'react';
 
-export default function TimeRow(){ 
+export default function TimeRow(){
+	let arr = [];
+	for(let i=0; i<8; i++){arr.push(i);}
+
+	const items = arr.map(item => (
+		<div key={item} className="timeRow__item">
+			<span className="timeRow__text">{item*=3}:00</span>
+			<span className="timeRow__line"></span>
+		</div>
+	));
+
 	return(
 		<div className="timeRow">
 			<div className="timeRow__item"></div>
 			<div className="timeRow__item timeRow__item--all">
 				<span>all day</span>
 			</div>
-			<div className="timeRow__item">
-				<span className="timeRow__text">00:00</span>
-				<span className="timeRow__line"></span>
-			</div>
-			<div className="timeRow__item">
-				<span className="timeRow__text">03:00</span>
-				<span className="timeRow__line"></span>
-			</div>
-			<div className="timeRow__item">
-				<span className="timeRow__text">06:00</span>
-				<span className="timeRow__line"></span>
-			</div>
-			<div className="timeRow__item">
-				<span className="timeRow__text">09:00</span>
-				<span className="timeRow__line"></span>
-			</div>
-			<div className="timeRow__item">
-				<span className="timeRow__text">12:00</span>
-				<span className="timeRow__line"></span>
-			</div>
-			<div className="timeRow__item">
-				<span className="timeRow__text">15:00</span>
-				<span className="timeRow__line"></span>
-			</div>
-			<div className="timeRow__item">
-				<span className="timeRow__text">18:00</span>
-				<span className="timeRow__line"></span>
-			</div>
-			<div className="timeRow__item">
-				<span className="timeRow__text">21:00</span>
-				<span className="timeRow__line"></span>
-			</div>
+
+			{items}
 		</div>
 	);
 }
